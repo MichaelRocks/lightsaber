3abee8548f61015a9078bf27aef4f02a811f3778
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2015-03-15 23:31:36 +0300

    Move processing logic from LightsaberProcessor to ClassFileVisitors

--- processor/src/main/java/com/michaelrocks/lightsaber/processor/LightsaberProcessor.java
+++ processor/src/main/java/com/michaelrocks/lightsaber/processor/LightsaberProcessor.java
@@ -18,20 +18,13 @@ package com.michaelrocks.lightsaber.processor;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
+import com.michaelrocks.lightsaber.processor.files.*;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
 
-import java.io.*;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.jar.JarOutputStream;
 
 public class LightsaberProcessor {
     private final LightsaberParameters parameters;
@@ -67,7 +60,7 @@ public class LightsaberProcessor {
                 processJarFileWithCopy(jarFile);
             } else if (parameters.classes != null) {
                 final File classesDirectory = new File(parameters.classes);
-                processClasses(classesDirectory);
+                processClassesWithCopy(classesDirectory);
             }
             System.out.println("DONE");
             return true;
@@ -80,86 +73,48 @@ public class LightsaberProcessor {
     }
 
     private void processJarFileWithCopy(final File file) throws ProcessingException {
-        final File lightsaberCopy = composeFileCopy(file, "lightsabered");
-        final File originalCopy = composeFileCopy(file, "original");
+        final File lightsaberCopy = composeCopyName(file, "lightsabered");
+        final File originalCopy = composeCopyName(file, "original");
         try {
-            processJarFile(file, lightsaberCopy);
+            final ClassFileReader<?> classFileReader = new JarClassFileReader(file);
+            final ClassFileWriter classFileWriter = new JarClassFileWriter(lightsaberCopy);
+            final InjectionClassFileVisitor classFileVisitor = new InjectionClassFileVisitor(classFileWriter);
+            classFileReader.accept(classFileVisitor);
             Files.move(file.toPath(), originalCopy.toPath(), StandardCopyOption.REPLACE_EXISTING);
             Files.move(lightsaberCopy.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
         } catch (final Exception exception) {
             // noinspection ResultOfMethodCallIgnored
             lightsaberCopy.delete();
+            throw new ProcessingException(exception);
         }
     }
 
-    private File composeFileCopy(final File file, final String copyName) {
+    private void processClassesWithCopy(final File directory) throws ProcessingException {
+        final File lightsaberCopy = composeCopyName(directory, "lightsabered");
+        final File originalCopy = composeCopyName(directory, "original");
+        try {
+            FileUtils.deleteDirectory(lightsaberCopy);
+            // noinspection ResultOfMethodCallIgnored
+            lightsaberCopy.mkdirs();
+            final ClassFileReader<?> classFileReader = new DirectoryClassFileReader(directory);
+            final ClassFileWriter classFileWriter = new DirectoryClassFileWriter(lightsaberCopy);
+            final InjectionClassFileVisitor classFileVisitor = new InjectionClassFileVisitor(classFileWriter);
+            classFileReader.accept(classFileVisitor);
+            FileUtils.deleteQuietly(originalCopy);
+            Files.move(directory.toPath(), originalCopy.toPath());
+            Files.move(lightsaberCopy.toPath(), directory.toPath());
+        } catch (final Exception exception) {
+            // noinspection ResultOfMethodCallIgnored
+            FileUtils.deleteQuietly(lightsaberCopy);
+            throw new ProcessingException(exception);
+        }
+    }
+
+    private File composeCopyName(final File file, final String copyName) {
         final File parentFile = file.getParentFile();
         final String fileNameWithExtension = file.getName();
         final String fileName = FilenameUtils.getBaseName(fileNameWithExtension);
         final String extension = FilenameUtils.getExtension(fileNameWithExtension);
-        return new File(parentFile, fileName + '.' + copyName + extension);
-    }
-
-    private void processJarFile(final File sourceFile, final File targetFile) throws ProcessingException {
-        System.out.println("Processing: " + sourceFile.getAbsolutePath());
-
-        JarFile jarFile = null;
-        JarOutputStream outputStream = null;
-        try {
-            jarFile = new JarFile(sourceFile, true);
-            outputStream = new JarOutputStream(new FileOutputStream(targetFile));
-            processJarFile(jarFile, outputStream);
-        } catch (final IOException exception) {
-            throw new ProcessingException(sourceFile, exception);
-        } finally {
-            IOUtils.closeQuietly(outputStream);
-            IOUtils.closeQuietly(jarFile);
-        }
-    }
-
-    private void processJarFile(final JarFile jarFile, final JarOutputStream jarOutputStream) throws IOException {
-        final Enumeration<JarEntry> entries = jarFile.entries();
-        while (entries.hasMoreElements()) {
-            final JarEntry entry = entries.nextElement();
-            jarOutputStream.putNextEntry(new JarEntry(entry.getName()));
-            try (final InputStream entryStream = jarFile.getInputStream(entry)) {
-                if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
-                    final byte[] classBytes = IOUtils.toByteArray(entryStream);
-                    processClass(classBytes, jarOutputStream);
-                } else {
-                    IOUtils.copy(entryStream, jarOutputStream);
-                }
-            }
-            jarOutputStream.closeEntry();
-        }
-    }
-
-    private void processClasses(final File classesDirectory) throws ProcessingException {
-        System.out.println("Processing: " + classesDirectory.getAbsolutePath());
-
-        if (!classesDirectory.exists() || !classesDirectory.isDirectory()) {
-            throw new ProcessingException(classesDirectory, "Invalid classes directory");
-        }
-
-        final Iterator<File> classFiles = FileUtils.iterateFiles(classesDirectory, new String[] { "class" }, true);
-        while (classFiles.hasNext()) {
-            final File classFile = classFiles.next();
-            try {
-                final byte[] classData = Files.readAllBytes(classFile.toPath());
-                try (final OutputStream outputStream = new FileOutputStream(classFile)) {
-                    processClass(classData, outputStream);
-                }
-            } catch (final IOException exception) {
-                throw new ProcessingException(classFile, exception);
-            }
-        }
-    }
-
-    private void processClass(final byte[] classBytes, final OutputStream outputStream) throws IOException {
-        final ClassReader classReader = new ClassReader(classBytes);
-        final ClassWriter classWriter =
-                new ClassWriter(classReader, ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
-        classReader.accept(new RootVisitor(classWriter), ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES);
-        outputStream.write(classWriter.toByteArray());
+        return new File(parentFile, fileName + '-' + copyName + extension);
     }
 }
