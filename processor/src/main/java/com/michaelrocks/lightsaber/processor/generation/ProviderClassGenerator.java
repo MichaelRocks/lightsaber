91ca43843798e703aac74f1b535a6d3edd95b7c8
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2015-04-25 14:50:30 +0300

    Generate providers as copyable

--- processor/src/main/java/com/michaelrocks/lightsaber/processor/generation/ProviderClassGenerator.java
+++ processor/src/main/java/com/michaelrocks/lightsaber/processor/generation/ProviderClassGenerator.java
@@ -16,6 +16,7 @@
 
 package com.michaelrocks.lightsaber.processor.generation;
 
+import com.michaelrocks.lightsaber.CopyableProvider;
 import com.michaelrocks.lightsaber.Injector;
 import com.michaelrocks.lightsaber.processor.ProcessorContext;
 import com.michaelrocks.lightsaber.processor.descriptors.MethodDescriptor;
@@ -35,6 +36,7 @@ public class ProviderClassGenerator {
     private static final String GET_METHOD_NAME = "get";
     private static final String GET_PROVIDER_METHOD_NAME = "getProvider";
     private static final String INJECT_MEMBERS_METHOD_NAME = "injectMembers";
+    private static final String COPY_WITH_INJECTOR_METHOD_NAME = "copyWithInjector";
 
     private final ProcessorContext processorContext;
     private final ProviderDescriptor provider;
@@ -52,12 +54,13 @@ public class ProviderClassGenerator {
                 provider.getProviderType().getInternalName(),
                 null,
                 Type.getInternalName(Object.class),
-                new String[] { Type.getInternalName(Provider.class) });
+                new String[] { Type.getInternalName(CopyableProvider.class) });
 
         generateModuleField(classWriter);
         generateInjectorField(classWriter);
         generateConstructor(classWriter);
         generateGetMethod(classWriter);
+        generateCopyWithInjector(classWriter);
 
         classWriter.visitEnd();
         return classWriter.toByteArray();
@@ -84,8 +87,7 @@ public class ProviderClassGenerator {
     }
 
     private void generateConstructor(final ClassWriter classWriter) {
-        final MethodDescriptor providerConstructor =
-                MethodDescriptor.forConstructor(provider.getModuleType(), Type.getType(Injector.class));
+        final MethodDescriptor providerConstructor = getProviderConstructor();
         final MethodVisitor methodVisitor = classWriter.visitMethod(
                 ACC_PUBLIC,
                 providerConstructor.getName(),
@@ -216,5 +218,38 @@ public class ProviderClassGenerator {
                 Type.getMethodDescriptor(
                         Type.VOID_TYPE, Type.getType(Injector.class), Type.getType(Object.class)),
                 false);
+    }
+
+    private void generateCopyWithInjector(final ClassWriter classWriter) {
+        final MethodVisitor methodVisitor = classWriter.visitMethod(
+                ACC_PUBLIC,
+                COPY_WITH_INJECTOR_METHOD_NAME,
+                Type.getMethodDescriptor(Type.getType(CopyableProvider.class), Type.getType(Injector.class)),
+                null,
+                null);
+        methodVisitor.visitCode();
+        methodVisitor.visitTypeInsn(NEW, provider.getProviderType().getInternalName());
+        methodVisitor.visitInsn(DUP);
+        methodVisitor.visitVarInsn(ALOAD, 0);
+        methodVisitor.visitFieldInsn(
+                GETFIELD,
+                provider.getProviderType().getInternalName(),
+                MODULE_FIELD_NAME,
+                provider.getModuleType().getDescriptor());
+        methodVisitor.visitVarInsn(ALOAD, 1);
+        final MethodDescriptor providerConstructor = getProviderConstructor();
+        methodVisitor.visitMethodInsn(
+                INVOKESPECIAL,
+                provider.getProviderType().getInternalName(),
+                providerConstructor.getName(),
+                providerConstructor.getDescriptor(),
+                false);
+        methodVisitor.visitInsn(ARETURN);
+        methodVisitor.visitMaxs(0, 0);
+        methodVisitor.visitEnd();
+    }
+
+    private MethodDescriptor getProviderConstructor() {
+        return MethodDescriptor.forConstructor(provider.getModuleType(), Type.getType(Injector.class));
     }
 }
