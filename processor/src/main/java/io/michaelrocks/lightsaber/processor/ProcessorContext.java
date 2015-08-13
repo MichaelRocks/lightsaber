ce577dd7b13b693c9fcc0da889cc2a0108552a5b
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2015-08-13 11:04:39 +0300

    Store qualifier types in ProcessorContext

--- processor/src/main/java/io/michaelrocks/lightsaber/processor/ProcessorContext.java
+++ processor/src/main/java/io/michaelrocks/lightsaber/processor/ProcessorContext.java
@@ -18,6 +18,7 @@ package io.michaelrocks.lightsaber.processor;
 
 import io.michaelrocks.lightsaber.SingletonProvider;
 import io.michaelrocks.lightsaber.internal.Lightsaber$$InjectorFactory;
+import io.michaelrocks.lightsaber.processor.annotations.AnnotationRegistry;
 import io.michaelrocks.lightsaber.processor.descriptors.FieldDescriptor;
 import io.michaelrocks.lightsaber.processor.descriptors.InjectionTargetDescriptor;
 import io.michaelrocks.lightsaber.processor.descriptors.InjectorDescriptor;
@@ -33,9 +34,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ProcessorContext {
     private static final String PACKAGE_MODULE_CLASS_NAME = "Lightsaber$$PackageModule";
@@ -46,12 +49,15 @@ public class ProcessorContext {
     private String classFilePath;
     private final Map<String, List<Exception>> errorsByPath = new LinkedHashMap<>();
 
+    private final AnnotationRegistry annotationRegistry = new AnnotationRegistry();
+
     private TypeGraph typeGraph;
     private final Map<Type, ModuleDescriptor> modules = new HashMap<>();
     private final Map<Type, ModuleDescriptor> packageModules = new HashMap<>();
     private final Map<Type, InjectionTargetDescriptor> injectableTargets = new HashMap<>();
     private final Map<Type, InjectionTargetDescriptor> providableTargets = new HashMap<>();
     private final Map<Type, InjectorDescriptor> injectors = new HashMap<>();
+    private final Set<Type> qualifiers = new HashSet<>();
 
     public String getClassFilePath() {
         return classFilePath;
@@ -80,6 +86,10 @@ public class ProcessorContext {
             errorsByPath.put(classFilePath, errors);
         }
         errors.add(error);
+    }
+
+    public AnnotationRegistry getAnnotationRegistry() {
+        return annotationRegistry;
     }
 
     public TypeGraph getTypeGraph() {
@@ -158,6 +168,14 @@ public class ProcessorContext {
         return Collections.singleton(SINGLETON_SCOPE_DESCRIPTOR);
     }
 
+    public void addQualifier(final Type type) {
+        qualifiers.add(type);
+    }
+
+    public boolean isQualifier(final Type type) {
+        return qualifiers.contains(type);
+    }
+
     public Type getPackageModuleType(final String packageName) {
         return Type.getObjectType(packageName + PACKAGE_MODULE_CLASS_NAME);
     }
@@ -191,6 +209,9 @@ public class ProcessorContext {
             for (final MethodDescriptor injectableConstructor : providableTarget.getInjectableConstructors()) {
                 System.out.println("\tConstructor: " + injectableConstructor);
             }
+        }
+        for (final Type qualifierType : qualifiers) {
+            System.out.println("\tQualifier: " + qualifierType);
         }
     }
 }
