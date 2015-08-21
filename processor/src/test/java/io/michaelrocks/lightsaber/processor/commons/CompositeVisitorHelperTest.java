4f896c1c3eee8f7875e9224151d6a5d9570e34a0
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2015-08-21 09:44:29 +0300

    Add unit tests for CompositeVisitorHelper

---
+++ processor/src/test/java/io/michaelrocks/lightsaber/processor/commons/CompositeVisitorHelperTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015 Michael Rozumyanskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.michaelrocks.lightsaber.processor.commons;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class CompositeVisitorHelperTest {
+    @Test
+    public void testAddNullVisitor() throws Exception {
+        // noinspection unchecked
+        final CompositeVisitor<Object> compositeVisitor = mock(CompositeVisitor.class);
+        CompositeVisitorHelper.addVisitor(compositeVisitor, null);
+        verifyZeroInteractions(compositeVisitor);
+    }
+
+    @Test
+    public void testAddNonNullVisitor() throws Exception {
+        // noinspection unchecked
+        final CompositeVisitor<Object> compositeVisitor = mock(CompositeVisitor.class);
+        final Object visitor = new Object();
+        CompositeVisitorHelper.addVisitor(compositeVisitor, visitor);
+        verify(compositeVisitor, only()).addVisitor(visitor);
+    }
+}
