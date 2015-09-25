e41eb17a14b31e27a848deedfa63f273f3b05cbd
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2015-09-25 09:44:23 +0300

    Split Lightsaber Gradle plugin into Java and Android plugins

---
+++ gradle-plugin/src/main/groovy/io/michaelrocks/lightsaber/plugin/AndroidLightsaberPlugin.groovy
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2015 Michael Rozumyanskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.michaelrocks.lightsaber.plugin
+
+import com.android.build.gradle.api.BaseVariant
+import com.android.build.transform.api.Transform
+import org.gradle.api.GradleException
+import org.gradle.api.Project
+import org.gradle.api.logging.LogLevel
+
+class AndroidLightsaberPlugin extends BaseLightsaberPlugin {
+    @Override
+    void apply(final Project project) {
+        super.apply(project)
+
+        if (project.hasProperty('android')) {
+            if (project.android.respondsTo('registerTransform', Transform, Object[])) {
+                final boolean verbose = logger.isEnabled(LogLevel.DEBUG)
+                project.android.registerTransform(new ProcessorTransform(verbose))
+            } else {
+                project.afterEvaluate {
+                    if (project.plugins.hasPlugin('com.android.application')) {
+                        setupLightsaberForLegacyAndroid(project.android.applicationVariants)
+                    } else if (project.plugins.hasPlugin('com.android.library')) {
+                        setupLightsaberForLegacyAndroid(project.android.libraryVariants)
+                    } else {
+                        throw new GradleException("Only application and library Android projects are supported")
+                    }
+                }
+            }
+        } else {
+            throw new GradleException("Lightsaber plugin must be applied *AFTER* Android plugin")
+        }
+    }
+
+    private void setupLightsaberForLegacyAndroid(final Collection<BaseVariant> variants) {
+        logger.info("Setting up Lightsaber task for Android project ${project.name}...")
+        variants.all { final BaseVariant variant ->
+            logger.trace("Creating Lightsaber tasks for variant ${variant.name}")
+            final String variantName = variant.name.capitalize()
+            final File classesDir = variant.javaCompiler.destinationDir
+            final File backupDir = new File(project.buildDir, "lightsaber/${variant.name}")
+            final List<File> classpath =
+                    project.android.bootClasspath.toList() + variant.javaCompiler.classpath.toList()
+            createTasks(classesDir, backupDir, classpath, variant.javaCompiler, variantName)
+        }
+    }
+}
--- gradle-plugin/src/main/groovy/io/michaelrocks/lightsaber/plugin/BaseLightsaberPlugin.groovy
+++ gradle-plugin/src/main/groovy/io/michaelrocks/lightsaber/plugin/BaseLightsaberPlugin.groovy
@@ -16,62 +16,35 @@
 
 package io.michaelrocks.lightsaber.plugin
 
-import com.android.build.gradle.api.BaseVariant
-import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.logging.Logger
+import org.gradle.api.tasks.compile.AbstractCompile
 
-class LightsaberPlugin implements Plugin<Project> {
-    private Project project
-    private Logger logger
+class BaseLightsaberPlugin implements Plugin<Project> {
+    Project project
+    Logger logger
 
     @Override
     void apply(final Project project) {
         this.project = project
         this.logger = project.logger
-
-        project.afterEvaluate {
-            if (project.plugins.hasPlugin('com.android.application')) {
-                setupLightsaberForAndroid(project.android.applicationVariants)
-            } else if (project.plugins.hasPlugin('com.android.library')) {
-                setupLightsaberForAndroid(project.android.libraryVariants)
-            } else if (project.plugins.hasPlugin('java')) {
-                setupLightsaberForJava()
-            } else {
-                throw new GradleException("Project should use either Android or Java plugin")
-            }
-        }
     }
 
-    private void setupLightsaberForAndroid(final Collection<BaseVariant> variants) {
-        logger.info("Setting up Lightsaber task for Android project ${project.name}...")
-        variants.all { final BaseVariant variant ->
-            logger.trace("Creating Lightsaber tasks for variant ${variant.name}")
-            final String variantName = variant.name.capitalize()
-            final String newTaskName = "lightsaberProcess$variantName"
-            final File classesDir = variant.javaCompiler.destinationDir
-            final File backupDir = new File(project.buildDir, "lightsaber/${variant.name}")
-            final List<File> classpath = project.android.bootClasspath.toList() + variant.javaCompiler.classpath.toList()
-            final LightsaberTask lightsaberProcess =
-                    createLightsaberProcessTask(newTaskName, classesDir, backupDir, classpath)
-            final BackupClassesTask backupTask =
-                    createBackupClassFilesTask("lightsaberBackupClasses$variantName", classesDir, backupDir)
-            configureTasks(lightsaberProcess, backupTask, variant.javaCompiler)
-        }
+    private boolean trySetupLightsaberForAndroid() {
+
     }
 
-    private void setupLightsaberForJava() {
-        logger.info("Setting up Lightsaber task for Java project ${project.name}...")
-        final File classesDir = project.sourceSets.main.output.classesDir
-        final File backupDir = new File(project.buildDir, "lightsaber")
-        final List<File> classpath = project.tasks.compileJava.classpath.toList()
-        final LightsaberTask lightsaberProcess =
-                createLightsaberProcessTask("lightsaberProcess", classesDir, backupDir, classpath)
+    void createTasks(final File classesDir, final File backupDir, final List<File> classpath,
+            final AbstractCompile compileTask, final String nameSuffix = "") {
+        final String suffix = nameSuffix.capitalize()
+        final LightsaberTask lightsaberTask =
+                createLightsaberProcessTask("lightsaberProcess$suffix", classesDir, backupDir, classpath)
         final BackupClassesTask backupTask =
-                createBackupClassFilesTask("lightsaberBackupClasses", classesDir, backupDir)
-        configureTasks(lightsaberProcess, backupTask, project.tasks.compileJava)
+                createBackupClassFilesTask("lightsaberBackupClasses$suffix", classesDir, backupDir)
+        configureTasks(lightsaberTask, backupTask, compileTask)
+
     }
 
     private void configureTasks(final LightsaberTask lightsaberTask, final BackupClassesTask backupTask,
