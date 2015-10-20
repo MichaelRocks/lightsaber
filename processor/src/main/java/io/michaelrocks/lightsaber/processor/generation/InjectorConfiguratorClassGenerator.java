c8b2e59d6cc7a623eac901c59244c3fdd77bbab6
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2015-10-20 11:13:22 +0300

    Add classes for generating injector configurators

---
+++ processor/src/main/java/io/michaelrocks/lightsaber/processor/generation/InjectorConfiguratorClassGenerator.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2015 Michael Rozumyanskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.michaelrocks.lightsaber.processor.generation;
+
+import io.michaelrocks.lightsaber.InjectorConfigurator;
+import io.michaelrocks.lightsaber.LightsaberInjector;
+import io.michaelrocks.lightsaber.processor.ProcessorContext;
+import io.michaelrocks.lightsaber.processor.annotations.AnnotationData;
+import io.michaelrocks.lightsaber.processor.annotations.proxy.AnnotationCreator;
+import io.michaelrocks.lightsaber.processor.commons.GeneratorAdapter;
+import io.michaelrocks.lightsaber.processor.commons.StandaloneClassWriter;
+import io.michaelrocks.lightsaber.processor.commons.Types;
+import io.michaelrocks.lightsaber.processor.descriptors.MethodDescriptor;
+import io.michaelrocks.lightsaber.processor.descriptors.ModuleDescriptor;
+import io.michaelrocks.lightsaber.processor.descriptors.ProviderDescriptor;
+import io.michaelrocks.lightsaber.processor.descriptors.QualifiedType;
+import io.michaelrocks.lightsaber.processor.watermark.WatermarkClassVisitor;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Type;
+
+import static org.objectweb.asm.Opcodes.*;
+
+public class InjectorConfiguratorClassGenerator {
+    private static final Type LIGHTSABER_INJECTOR_TYPE = Type.getType(LightsaberInjector.class);
+
+    private static final MethodDescriptor KEY_CONSTRUCTOR =
+            MethodDescriptor.forConstructor(Types.CLASS_TYPE, Types.ANNOTATION_TYPE);
+    private static final MethodDescriptor CONFIGURE_INJECTOR_METHOD =
+            MethodDescriptor.forMethod("configureInjector",
+                    Type.VOID_TYPE, LIGHTSABER_INJECTOR_TYPE, Types.OBJECT_TYPE);
+    private static final MethodDescriptor REGISTER_PROVIDER_METHOD =
+            MethodDescriptor.forMethod("registerProvider", Type.VOID_TYPE, Types.KEY_TYPE, Types.PROVIDER_TYPE);
+
+    private static final MethodDescriptor DELEGATE_PROVIDER_CONSTRUCTOR =
+            MethodDescriptor.forConstructor(Types.PROVIDER_TYPE);
+
+    private final ProcessorContext processorContext;
+    private final AnnotationCreator annotationCreator;
+    private final ModuleDescriptor module;
+
+    public InjectorConfiguratorClassGenerator(final ProcessorContext processorContext,
+            final AnnotationCreator annotationCreator, final ModuleDescriptor module) {
+        this.processorContext = processorContext;
+        this.annotationCreator = annotationCreator;
+        this.module = module;
+    }
+
+    public byte[] generate() {
+        final ClassWriter classWriter =
+                new StandaloneClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS, processorContext);
+        final ClassVisitor classVisitor = new WatermarkClassVisitor(classWriter, true);
+        classVisitor.visit(
+                V1_6,
+                ACC_PUBLIC | ACC_SUPER,
+                module.getConfiguratorType().getInternalName(),
+                null,
+                Type.getInternalName(Object.class),
+                new String[] { Type.getInternalName(InjectorConfigurator.class) });
+
+        generateConstructor(classVisitor);
+        generateConfigureInjectorMethod(classVisitor);
+
+        classVisitor.visitEnd();
+        return classWriter.toByteArray();
+    }
+
+    private void generateConstructor(final ClassVisitor classVisitor) {
+        final GeneratorAdapter generator =
+                new GeneratorAdapter(classVisitor, ACC_PUBLIC, MethodDescriptor.forDefaultConstructor());
+        generator.visitCode();
+        generator.loadThis();
+        generator.dup();
+        generator.invokeConstructor(Types.OBJECT_TYPE, MethodDescriptor.forDefaultConstructor());
+        generator.returnValue();
+        generator.endMethod();
+    }
+
+    private void generateConfigureInjectorMethod(final ClassVisitor classVisitor) {
+        final GeneratorAdapter generator = new GeneratorAdapter(classVisitor, ACC_PUBLIC, CONFIGURE_INJECTOR_METHOD);
+        generator.visitCode();
+
+        generator.loadArg(1);
+        generator.checkCast(module.getModuleType());
+        final int moduleLocal = generator.newLocal(module.getModuleType());
+        generator.storeLocal(moduleLocal);
+
+        for (final ProviderDescriptor provider : module.getProviders()) {
+            generateRegisterProviderInvocation(generator, provider, moduleLocal);
+        }
+
+        generator.returnValue();
+        generator.endMethod();
+    }
+
+    private void generateRegisterProviderInvocation(final GeneratorAdapter generator,
+            final ProviderDescriptor provider, final int moduleLocal) {
+        generator.loadArg(0);
+        generateKeyConstruction(generator, provider);
+
+        if (provider.getDelegatorType() != null) {
+            generateDelegatorConstruction(generator, provider, moduleLocal);
+        } else {
+            generateProviderConstruction(generator, provider, moduleLocal);
+        }
+
+        generator.invokeVirtual(LIGHTSABER_INJECTOR_TYPE, REGISTER_PROVIDER_METHOD);
+    }
+
+    private void generateKeyConstruction(final GeneratorAdapter generator, final ProviderDescriptor provider) {
+        generator.newInstance(Types.KEY_TYPE);
+        generator.dup();
+        final QualifiedType providableType = provider.getQualifiedProvidableType();
+        generator.push(Types.box(providableType.getType()));
+        final AnnotationData qualifier = providableType.getQualifier();
+        if (qualifier == null) {
+            generator.pushNull();
+        } else {
+            annotationCreator.newAnnotation(generator, qualifier);
+        }
+        generator.invokeConstructor(Types.KEY_TYPE, KEY_CONSTRUCTOR);
+    }
+
+    private void generateDelegatorConstruction(final GeneratorAdapter generator, final ProviderDescriptor provider,
+            final int moduleLocal) {
+        final Type delegatorType = provider.getDelegatorType();
+        generator.newInstance(delegatorType);
+        generator.dup();
+        generateProviderConstruction(generator, provider, moduleLocal);
+        generator.invokeConstructor(delegatorType, DELEGATE_PROVIDER_CONSTRUCTOR);
+    }
+
+    private void generateProviderConstruction(final GeneratorAdapter generator, final ProviderDescriptor provider,
+            final int moduleLocal) {
+        generator.newInstance(provider.getProviderType());
+        generator.dup();
+        generator.loadLocal(moduleLocal);
+        generator.loadArg(0);
+        final MethodDescriptor constructor =
+                MethodDescriptor.forConstructor(provider.getModuleType(), Types.INJECTOR_TYPE);
+        generator.invokeConstructor(provider.getProviderType(), constructor);
+    }
+}
---
+++ processor/src/main/java/io/michaelrocks/lightsaber/processor/generation/InjectorConfiguratorsGenerator.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015 Michael Rozumyanskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.michaelrocks.lightsaber.processor.generation;
+
+import io.michaelrocks.lightsaber.processor.ProcessorContext;
+import io.michaelrocks.lightsaber.processor.annotations.proxy.AnnotationCreator;
+import io.michaelrocks.lightsaber.processor.descriptors.ModuleDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InjectorConfiguratorsGenerator {
+    private static final Logger logger = LoggerFactory.getLogger(InjectorConfiguratorsGenerator.class);
+
+    private final ClassProducer classProducer;
+    private final ProcessorContext processorContext;
+    private final AnnotationCreator annotationCreator;
+
+    public InjectorConfiguratorsGenerator(final ClassProducer classProducer, final ProcessorContext processorContext,
+            final AnnotationCreator annotationCreator) {
+        this.classProducer = classProducer;
+        this.processorContext = processorContext;
+        this.annotationCreator = annotationCreator;
+    }
+
+    public void generateInjectorConfigurators() {
+        for (final ModuleDescriptor module : processorContext.getModules()) {
+            generateInjectorConfigurator(module);
+        }
+    }
+
+    private void generateInjectorConfigurator(final ModuleDescriptor module) {
+        logger.debug("Generating injector configurator {}", module.getConfiguratorType().getInternalName());
+        final InjectorConfiguratorClassGenerator generator =
+                new InjectorConfiguratorClassGenerator(processorContext, annotationCreator, module);
+        final byte[] classData = generator.generate();
+        classProducer.produceClass(module.getConfiguratorType().getInternalName(), classData);
+    }
+}
