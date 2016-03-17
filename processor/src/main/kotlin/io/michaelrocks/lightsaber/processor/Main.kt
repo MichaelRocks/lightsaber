eec7185d060fb9e4be7ad78aedec32d95a3fd883
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2016-03-17 13:31:05 +0300

    A library is not necessarily a file

--- processor/src/main/kotlin/io/michaelrocks/lightsaber/processor/Main.kt
+++ processor/src/main/kotlin/io/michaelrocks/lightsaber/processor/Main.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Michael Rozumyanskiy
+ * Copyright 2016 Michael Rozumyanskiy
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -84,9 +84,6 @@ private fun validateLibraries(libraries: List<File>?) {
   for (library in libraries) {
     if (!library.exists()) {
       throw ParameterException("Library doesn't exist: " + library)
-    }
-    if (!library.isFile) {
-      throw ParameterException("Library is not a file: " + library)
     }
   }
 }
