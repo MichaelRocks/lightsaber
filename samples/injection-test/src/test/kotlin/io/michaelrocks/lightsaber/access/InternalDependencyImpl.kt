0606323be4aabf0078923ccd5f8350905057a5bf
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2016-03-21 00:30:09 +0300

    Fix formatting

--- samples/injection-test/src/test/kotlin/io/michaelrocks/lightsaber/access/InternalDependencyImpl.kt
+++ samples/injection-test/src/test/kotlin/io/michaelrocks/lightsaber/access/InternalDependencyImpl.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Michael Rozumyanskiy
+ * Copyright 2016 Michael Rozumyanskiy
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,9 +18,7 @@ package io.michaelrocks.lightsaber.access
 
 import javax.inject.Inject
 
-internal class InternalDependencyImpl @Inject
-private constructor() : InternalDependency {
-
+internal class InternalDependencyImpl @Inject private constructor() : InternalDependency {
   override fun action() {
   }
 
