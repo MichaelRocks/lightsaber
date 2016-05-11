675925d572de910250be6bb514fafa73c0320992
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2016-05-11 19:48:02 +0300

    Perform validation of the injection graph

--- processor/src/main/kotlin/io/michaelrocks/lightsaber/processor/validation/Validator.kt
+++ processor/src/main/kotlin/io/michaelrocks/lightsaber/processor/validation/Validator.kt
@@ -18,8 +18,10 @@ package io.michaelrocks.lightsaber.processor.validation
 
 import io.michaelrocks.grip.ClassRegistry
 import io.michaelrocks.lightsaber.processor.ErrorReporter
+import io.michaelrocks.lightsaber.processor.graph.DirectedGraph
 import io.michaelrocks.lightsaber.processor.graph.findCycles
 import io.michaelrocks.lightsaber.processor.graph.findMissingVertices
+import io.michaelrocks.lightsaber.processor.graph.reversed
 import io.michaelrocks.lightsaber.processor.model.InjectionContext
 
 class Validator(
@@ -30,6 +32,7 @@ class Validator(
     performSanityChecks(context)
     validateDependencyGraph(context)
     validateComponentGraph(context)
+    validateInjectionGraph(context)
   }
 
   private fun performSanityChecks(context: InjectionContext) {
@@ -55,6 +58,50 @@ class Validator(
     val cycles = componentGraph.findCycles()
     for (cycle in cycles) {
       errorReporter.reportError("Cycled component: ${cycle.joinToString(" -> ")}")
+    }
+  }
+
+  private fun validateInjectionGraph(context: InjectionContext) {
+    val injectionGraphs = buildInjectionGraphs(context)
+    injectionGraphs.forEach { validateNoDuplicatesInInjectionGraph(it) }
+
+    val injectionGraph = buildInjectionGraph(injectionGraphs)
+    context.components.forEach { component ->
+      if (InjectionGraphVertex.ComponentVertex(component) !in injectionGraph) {
+        errorReporter.reportError("Abandoned component: ${component.type.className}")
+      }
+    }
+  }
+
+  private fun validateNoDuplicatesInInjectionGraph(graph: DirectedGraph<InjectionGraphVertex>) {
+    val reversed = graph.reversed()
+    reversed.vertices.forEach { vertex ->
+      val adjacent = reversed.getAdjacentVertices(vertex)
+      if (adjacent != null && adjacent.size > 1) {
+        when (vertex) {
+          is InjectionGraphVertex.ComponentVertex -> {}
+          is InjectionGraphVertex.ModuleVertex -> {
+            val module = vertex.module.type.className
+            val components =
+                adjacent
+                    .mapNotNull { it as? InjectionGraphVertex.ComponentVertex }
+                    .joinToString { it.component.type.className }
+            errorReporter.reportError(
+                "Module provided multiple times in a component chain: $module in $components"
+            )
+          }
+          is InjectionGraphVertex.DependencyVertex -> {
+            val dependency = vertex.dependency.toString()
+            val modules =
+                adjacent
+                    .mapNotNull { it as? InjectionGraphVertex.ModuleVertex }
+                    .joinToString { it.module.type.className }
+            errorReporter.reportError(
+                "Dependency provided multiple times in a component chain: $dependency: $modules"
+            )
+          }
+        }
+      }
     }
   }
 }
