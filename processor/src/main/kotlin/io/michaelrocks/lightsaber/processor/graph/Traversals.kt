de904dd4abc9d58c59c1bb2441cc3570f451411e
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2016-05-14 18:17:26 +0300

    Provide classes for graph traversal

--- processor/src/main/kotlin/io/michaelrocks/lightsaber/processor/graph/Traversals.kt
+++ processor/src/main/kotlin/io/michaelrocks/lightsaber/processor/graph/Traversals.kt
@@ -18,6 +18,76 @@ package io.michaelrocks.lightsaber.processor.graph
 
 import java.util.*
 
+interface Traversal<T, D : Traversal.Delegate<T>> {
+  fun traverse(graph: DirectedGraph<T>, delegate: D)
+  fun traverse(graph: DirectedGraph<T>, delegate: D, fromVertex: T)
+
+  interface Delegate<T>
+}
+
+abstract class AbstractTraversal<T, D : Traversal.Delegate<T>> : Traversal<T, D> {
+  override fun traverse(graph: DirectedGraph<T>, delegate: D) {
+    graph.vertices.forEach { performTraversal(graph, delegate, it) }
+  }
+
+  override fun traverse(graph: DirectedGraph<T>, delegate: D, fromVertex: T) {
+    performTraversal(graph, delegate, fromVertex)
+  }
+
+  protected abstract fun performTraversal(graph: DirectedGraph<T>, delegate: D, vertex: T)
+}
+
+abstract class AbstractMarkingTraversal<T> : AbstractTraversal<T, AbstractMarkingTraversal.Delegate<T>>() {
+  interface Delegate<T> : Traversal.Delegate<T> {
+    fun onBeforeVertex(vertex: T) = Unit
+    fun onAfterVertex(vertex: T) = Unit
+    fun onBeforeAdjacentVertices(vertex: T) = Unit
+    fun onAfterAdjacentVertices(vertex: T) = Unit
+
+    fun isVisited(vertex: T): Boolean
+    fun markAsVisited(vertex: T)
+  }
+
+  open class SimpleDelegate<T> : Delegate<T> {
+    private val visited = HashSet<T>()
+
+    fun getVisitedVertices(): Set<T> {
+      return HashSet(visited)
+    }
+
+    override fun isVisited(vertex: T): Boolean {
+      return vertex in visited
+    }
+
+    override fun markAsVisited(vertex: T) {
+      visited += vertex
+    }
+
+    open fun reset() {
+      visited.clear()
+    }
+  }
+}
+
+class DepthFirstTraversal<T> : AbstractMarkingTraversal<T>() {
+  override fun performTraversal(graph: DirectedGraph<T>, delegate: Delegate<T>, vertex: T) {
+    delegate.onBeforeVertex(vertex)
+    try {
+      if (!delegate.isVisited(vertex)) {
+        delegate.markAsVisited(vertex)
+        delegate.onBeforeAdjacentVertices(vertex)
+        try {
+          graph.getAdjacentVertices(vertex)?.forEach { performTraversal(graph, delegate, it) }
+        } finally {
+          delegate.onAfterAdjacentVertices(vertex)
+        }
+      }
+    } finally {
+      delegate.onAfterVertex(vertex)
+    }
+  }
+}
+
 fun <T> DirectedGraph<T>.traverseDepthFirst(
     beforeAdjacent: (T) -> Unit = {},
     afterAdjacent: (T) -> Unit = {}
@@ -34,6 +104,4 @@ fun <T> DirectedGraph<T>.traverseDepthFirst(
       }
     }
   }
-
-  vertices.forEach { traverse(it) }
 }
