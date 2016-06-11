d99eaa88dab4756e07e44ea5d33acad9e79bdf1b
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2016-06-11 23:01:18 +0300

    Request TESTED_CODE in referenced scopes

--- gradle-plugin/src/main/groovy/io/michaelrocks/lightsaber/plugin/LightsaberTransform.groovy
+++ gradle-plugin/src/main/groovy/io/michaelrocks/lightsaber/plugin/LightsaberTransform.groovy
@@ -83,6 +83,7 @@ public class LightsaberTransform extends Transform {
         QualifiedContent.Scope.SUB_PROJECTS,
         QualifiedContent.Scope.SUB_PROJECTS_LOCAL_DEPS,
         QualifiedContent.Scope.EXTERNAL_LIBRARIES,
+        QualifiedContent.Scope.TESTED_CODE,
         QualifiedContent.Scope.PROVIDED_ONLY
     )
   }
