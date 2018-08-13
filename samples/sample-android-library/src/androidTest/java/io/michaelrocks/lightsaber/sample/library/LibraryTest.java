d5ee7c931f0af5518b3b6100c5bf1e1ab3051abb
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2018-08-13 09:50:33 +0300

    Fix warnings in LibraryTest

--- samples/sample-android-library/src/androidTest/java/io/michaelrocks/lightsaber/sample/library/LibraryTest.java
+++ samples/sample-android-library/src/androidTest/java/io/michaelrocks/lightsaber/sample/library/LibraryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Michael Rozumyanskiy
+ * Copyright 2018 Michael Rozumyanskiy
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,11 +21,11 @@ import android.os.Looper;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.annotation.UiThreadTest;
 import android.support.test.filters.SmallTest;
-import android.support.test.rule.UiThreadTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import org.junit.Rule;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
@@ -33,20 +33,15 @@ import static org.junit.Assert.assertSame;
 @SmallTest
 @RunWith(AndroidJUnit4.class)
 public class LibraryTest {
-  @Rule
-  public UiThreadTestRule getUiThreadTestRule() {
-    return new UiThreadTestRule();
-  }
-
   @Test
-  public void checkApplicationContext() throws Exception {
+  public void checkApplicationContext() {
     final Context context = InstrumentationRegistry.getTargetContext();
     assertEquals("io.michaelrocks.lightsaber.sample.library.test", context.getPackageName());
   }
 
   @Test
   @UiThreadTest
-  public void checkUiThread() throws Exception {
+  public void checkUiThread() {
     assertSame(Looper.getMainLooper(), Looper.myLooper());
   }
 }
