3644f0a069671d1f5e4f374b218dcdee4a1b9c25
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2019-04-14 14:06:49 +0200

    Add a builder for creating provider interceptors

---
+++ core-test/src/main/java/io/michaelrocks/lightsaber/ProviderInterceptorBuilder.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2019 Michael Rozumyanskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.michaelrocks.lightsaber;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.inject.Provider;
+
+public class ProviderInterceptorBuilder {
+  private final Map<Key<?>, Provider<?>> providers = new HashMap<Key<?>, Provider<?>>();
+
+  public <T> ProviderInterceptorBuilder addProviderForClass(@Nonnull final Class<T> type, @Nonnull final Provider<? extends T> provider) {
+    return addProviderInternal(Key.of(type), provider);
+  }
+
+  public <T> ProviderInterceptorBuilder addProviderForClass(@Nonnull final Class<T> type, @Nullable final Annotation annotation,
+      @Nonnull final Provider<? extends T> provider) {
+    return addProviderInternal(Key.of(type, annotation), provider);
+  }
+
+  public <T> ProviderInterceptorBuilder addProviderForClass(@Nonnull final Class<T> type, @Nullable final Class<? extends Annotation> annotationClass,
+      @Nonnull final Provider<? extends T> provider) {
+    return addProviderInternal(Key.of(type, createAnnotation(annotationClass)), provider);
+  }
+
+  public ProviderInterceptorBuilder addProviderForType(@Nonnull final Type type, @Nullable final Annotation annotation,
+      @Nonnull final Provider<?> provider) {
+    return addProviderInternal(Key.of(type, annotation), provider);
+  }
+
+  public ProviderInterceptorBuilder addProviderForType(@Nonnull final Type type, @Nullable final Class<? extends Annotation> annotationClass,
+      @Nonnull final Provider<?> provider) {
+    return addProviderInternal(Key.of(type, createAnnotation(annotationClass)), provider);
+  }
+
+  public ProviderInterceptorBuilder addProviderForType(@Nonnull final Type type, @Nonnull final Provider<?> provider) {
+    return addProviderInternal(Key.of(type), provider);
+  }
+
+  public <T> ProviderInterceptorBuilder addProviderForKey(@Nonnull final Key<T> key, @Nonnull final Provider<? extends T> provider) {
+    return addProviderInternal(key, provider);
+  }
+
+  @Nonnull
+  public ProviderInterceptor build() {
+    return new ImmutableProviderInterceptor(this);
+  }
+
+  @Nullable
+  private Annotation createAnnotation(@Nullable final Class<? extends Annotation> annotationClass) {
+    return annotationClass != null ? new AnnotationBuilder<Annotation>(annotationClass).build() : null;
+  }
+
+  private ProviderInterceptorBuilder addProviderInternal(@Nonnull final Key<?> key, @Nonnull final Provider<?> provider) {
+    final Provider<?> oldProvider = providers.put(key, provider);
+    if (oldProvider != null) {
+      throw new IllegalArgumentException("Provider for key " + key + " already exists");
+    }
+
+    return this;
+  }
+
+  private class ImmutableProviderInterceptor implements ProviderInterceptor {
+    private final Map<Key<?>, Provider<?>> providers;
+
+    ImmutableProviderInterceptor(@Nonnull final ProviderInterceptorBuilder builder) {
+      providers = new HashMap<Key<?>, Provider<?>>(builder.providers);
+    }
+
+    @Nonnull
+    @Override
+    public Provider<?> intercept(@Nonnull final Chain chain, @Nonnull final Key<?> key) {
+      final Provider<?> provider = providers.get(key);
+      if (provider != null) {
+        return provider;
+      }
+
+      return chain.proceed(key);
+    }
+  }
+}
---
+++ core-test/src/test/java/io/michaelrocks/lightsaber/ProviderInterceptorBuilderTest.java
@@ -0,0 +1,364 @@
+/*
+ * Copyright 2019 Michael Rozumyanskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.michaelrocks.lightsaber;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+
+import javax.inject.Provider;
+
+
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+public class ProviderInterceptorBuilderTest {
+  @Test
+  public void testAddProviderForClass() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    final ProviderInterceptor interceptor = new ProviderInterceptorBuilder()
+        .addProviderForClass(String.class, provider)
+        .build();
+
+    assertIntercepted(interceptor, Key.of(String.class), provider);
+    assertNotIntercepted(interceptor, Key.of(Object.class));
+  }
+
+  @Test
+  public void testAddProviderForType() {
+    // noinspection unchecked
+    final Provider<List<String>> provider = mock(Provider.class);
+    final ProviderInterceptor interceptor = new ProviderInterceptorBuilder()
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), provider)
+        .build();
+
+    assertIntercepted(interceptor, Key.of(new TypeToken<List<String>>() {}.getType()), provider);
+    assertNotIntercepted(interceptor, Key.of(new TypeToken<List<Object>>() {}.getType()));
+  }
+
+  @Test
+  public void testAddProviderForKey() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    final ProviderInterceptor interceptor = new ProviderInterceptorBuilder()
+        .addProviderForKey(Key.of(String.class), provider)
+        .build();
+
+    assertIntercepted(interceptor, Key.of(String.class), provider);
+    assertNotIntercepted(interceptor, Key.of(Object.class));
+  }
+
+  @Test
+  public void testAddProviderForClassWithAnnotation() {
+    // noinspection unchecked
+    final Provider<String> provider1 = mock(Provider.class);
+    // noinspection unchecked
+    final Provider<String> provider2 = mock(Provider.class);
+    // noinspection unchecked
+    final Provider<String> provider3 = mock(Provider.class);
+
+    final ProviderInterceptor interceptor = new ProviderInterceptorBuilder()
+        .addProviderForClass(String.class, provider1)
+        .addProviderForClass(String.class, Annotated.class, provider2)
+        .addProviderForClass(String.class, createAnnotation("explicit"), provider3)
+        .build();
+
+    assertIntercepted(interceptor, Key.of(String.class), provider1);
+    assertIntercepted(interceptor, Key.of(String.class, createAnnotation()), provider2);
+    assertIntercepted(interceptor, Key.of(String.class, createAnnotation("explicit")), provider3);
+    assertNotIntercepted(interceptor, Key.of(Object.class));
+    assertNotIntercepted(interceptor, Key.of(Object.class, createAnnotation()));
+    assertNotIntercepted(interceptor, Key.of(Object.class, createAnnotation("explicit")));
+  }
+
+  @Test
+  public void testAddProviderForTypeWithAnnotation() {
+    // noinspection unchecked
+    final Provider<List<String>> provider1 = mock(Provider.class);
+    // noinspection unchecked
+    final Provider<List<String>> provider2 = mock(Provider.class);
+    // noinspection unchecked
+    final Provider<List<String>> provider3 = mock(Provider.class);
+
+    final ProviderInterceptor interceptor = new ProviderInterceptorBuilder()
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), provider1)
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), Annotated.class, provider2)
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), createAnnotation("explicit"), provider3)
+        .build();
+
+    assertIntercepted(interceptor, Key.of(new TypeToken<List<String>>() {}.getType()), provider1);
+    assertIntercepted(interceptor, Key.of(new TypeToken<List<String>>() {}.getType(), createAnnotation()), provider2);
+    assertIntercepted(interceptor, Key.of(new TypeToken<List<String>>() {}.getType(), createAnnotation("explicit")), provider3);
+    assertNotIntercepted(interceptor, Key.of(new TypeToken<List<Object>>() {}.getType()));
+    assertNotIntercepted(interceptor, Key.of(new TypeToken<List<Object>>() {}.getType(), createAnnotation()));
+    assertNotIntercepted(interceptor, Key.of(new TypeToken<List<Object>>() {}.getType(), createAnnotation("explicit")));
+  }
+
+  @Test
+  public void testAddProviderForKeyWithAnnotation() {
+    // noinspection unchecked
+    final Provider<String> provider1 = mock(Provider.class);
+    // noinspection unchecked
+    final Provider<String> provider2 = mock(Provider.class);
+    // noinspection unchecked
+    final Provider<String> provider3 = mock(Provider.class);
+
+    final ProviderInterceptor interceptor = new ProviderInterceptorBuilder()
+        .addProviderForKey(Key.of(String.class), provider1)
+        .addProviderForKey(Key.of(String.class, createAnnotation()), provider2)
+        .addProviderForKey(Key.of(String.class, createAnnotation("explicit")), provider3)
+        .build();
+
+    assertIntercepted(interceptor, Key.of(String.class), provider1);
+    assertIntercepted(interceptor, Key.of(String.class, createAnnotation()), provider2);
+    assertIntercepted(interceptor, Key.of(String.class, createAnnotation("explicit")), provider3);
+    assertNotIntercepted(interceptor, Key.of(Object.class));
+    assertNotIntercepted(interceptor, Key.of(Object.class, createAnnotation()));
+    assertNotIntercepted(interceptor, Key.of(Object.class, createAnnotation("explicit")));
+  }
+
+  @Test
+  public void testAddProviderForClassTypeAndKeyWithAnnotation() {
+    // noinspection unchecked
+    final Provider<String> provider1 = mock(Provider.class);
+    // noinspection unchecked
+    final Provider<String> provider2 = mock(Provider.class);
+    // noinspection unchecked
+    final Provider<String> provider3 = mock(Provider.class);
+    // noinspection unchecked
+    final Provider<String> provider4 = mock(Provider.class);
+    // noinspection unchecked
+    final Provider<String> provider5 = mock(Provider.class);
+    // noinspection unchecked
+    final Provider<String> provider6 = mock(Provider.class);
+
+    final ProviderInterceptor interceptor = new ProviderInterceptorBuilder()
+        .addProviderForClass(String.class, provider1)
+        .addProviderForClass(String.class, Annotated.class, provider2)
+        .addProviderForKey(Key.of(String.class, createAnnotation("explicit")), provider3)
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), provider4)
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), Annotated.class, provider5)
+        .addProviderForKey(Key.of(new TypeToken<List<String>>() {}.getType(), createAnnotation("explicit")), provider6)
+        .build();
+
+    assertIntercepted(interceptor, Key.of(String.class), provider1);
+    assertIntercepted(interceptor, Key.of(String.class, createAnnotation()), provider2);
+    assertIntercepted(interceptor, Key.of(String.class, createAnnotation("explicit")), provider3);
+    assertIntercepted(interceptor, Key.of(new TypeToken<List<String>>() {}.getType()), provider4);
+    assertIntercepted(interceptor, Key.of(new TypeToken<List<String>>() {}.getType(), createAnnotation()), provider5);
+    assertIntercepted(interceptor, Key.of(new TypeToken<List<String>>() {}.getType(), createAnnotation("explicit")), provider6);
+    assertNotIntercepted(interceptor, Key.of(Object.class));
+    assertNotIntercepted(interceptor, Key.of(Object.class, createAnnotation()));
+    assertNotIntercepted(interceptor, Key.of(Object.class, createAnnotation("explicit")));
+    assertNotIntercepted(interceptor, Key.of(new TypeToken<List<Object>>() {}.getType()));
+    assertNotIntercepted(interceptor, Key.of(new TypeToken<List<Object>>() {}.getType(), createAnnotation()));
+    assertNotIntercepted(interceptor, Key.of(new TypeToken<List<Object>>() {}.getType(), createAnnotation("explicit")));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForSameClassThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForClass(String.class, provider)
+        .addProviderForClass(String.class, provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForSameClassWithImplicitAnnotationThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForClass(String.class, Annotated.class, provider)
+        .addProviderForClass(String.class, Annotated.class, provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForSameClassWithExplicitAnnotationThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForClass(String.class, createAnnotation("explicit"), provider)
+        .addProviderForClass(String.class, createAnnotation("explicit"), provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForSameTypeThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), provider)
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForSameTypeWithImplicitAnnotationThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), Annotated.class, provider)
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), Annotated.class, provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForSameTypeWithExplicitAnnotationThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), createAnnotation("explicit"), provider)
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), createAnnotation("explicit"), provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForSameKeyThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForKey(Key.of(String.class), provider)
+        .addProviderForKey(Key.of(String.class), provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForSameKeyWithExplicitAnnotationThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForKey(Key.of(String.class, createAnnotation("explicit")), provider)
+        .addProviderForKey(Key.of(String.class, createAnnotation("explicit")), provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForClassAndEqualKeyThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForClass(String.class, provider)
+        .addProviderForKey(Key.of(String.class), provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForClassAndEqualKeyWithImplicitAnnotationThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForClass(String.class, Annotated.class, provider)
+        .addProviderForKey(Key.of(String.class, createAnnotation()), provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForClassAndEqualKeyWithExplicitAnnotationThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForClass(String.class, createAnnotation("explicit"), provider)
+        .addProviderForKey(Key.of(String.class, createAnnotation("explicit")), provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForTypeAndEqualKeyThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), provider)
+        .addProviderForKey(Key.of(new TypeToken<List<String>>() {}.getType()), provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForTypeAndEqualKeyWithImplicitAnnotationThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), Annotated.class, provider)
+        .addProviderForKey(Key.of(new TypeToken<List<String>>() {}.getType(), createAnnotation()), provider)
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAddProviderForTypeAndEqualKeyWithExplicitAnnotationThrowsException() {
+    // noinspection unchecked
+    final Provider<String> provider = mock(Provider.class);
+    new ProviderInterceptorBuilder()
+        .addProviderForType(new TypeToken<List<String>>() {}.getType(), createAnnotation("explicit"), provider)
+        .addProviderForKey(Key.of(new TypeToken<List<String>>() {}.getType(), createAnnotation("explicit")), provider)
+        .build();
+  }
+
+  private Annotated createAnnotation() {
+    return new AnnotationBuilder<Annotated>(Annotated.class).build();
+  }
+
+  @SuppressWarnings("SameParameterValue")
+  private Annotated createAnnotation(final String value) {
+    return new AnnotationBuilder<Annotated>(Annotated.class).addMember("value", value).build();
+  }
+
+  private void assertIntercepted(final ProviderInterceptor interceptor, final Key<?> key, final Provider<?> expectedProvider) {
+    final ProviderInterceptor.Chain chain = mock(ProviderInterceptor.Chain.class);
+    assertSame(expectedProvider, interceptor.intercept(chain, key));
+    verifyZeroInteractions(chain);
+  }
+
+  private void assertNotIntercepted(final ProviderInterceptor interceptor, final Key<?> key) {
+    final Provider<?> provider = mock(Provider.class);
+    final ProviderInterceptor.Chain chain = mock(ProviderInterceptor.Chain.class);
+    when(chain.proceed(Mockito.<Key<Object>>any())).thenAnswer(
+        new Answer<Provider<?>>() {
+          @Override
+          public Provider<?> answer(final InvocationOnMock invocation) {
+            return provider;
+          }
+        }
+    );
+
+    assertSame(provider, interceptor.intercept(chain, key));
+    verify(chain, only()).proceed(key);
+  }
+
+  @SuppressWarnings("unused")
+  private static abstract class TypeToken<T> {
+    Type getType() {
+      return ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private @interface Annotated {
+    String value() default "default";
+  }
+}
